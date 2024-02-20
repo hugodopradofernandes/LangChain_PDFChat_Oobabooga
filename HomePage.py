fcd46a5075d9d2200e1a62d69c3cbd4817d41fe8
Hugo do Prado <hugodopradofernandes@gmail.com>
2024-02-20 07:20:32 -0300

    Update and rename app.py to HomePage.py

---
+++ HomePage.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+# Main page setup
+st.set_page_config(page_title="Ask your PDF or Wikipedia")
+st.header("Ask your PDF or Wikipedia 💬")
+st.write("Select a page on the side menu")
+st.sidebar.success("Select a page above.")
