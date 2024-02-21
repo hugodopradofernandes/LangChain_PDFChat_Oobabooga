36b9a1f3c5b42c55b2b66ee0dd34c6086f8d49bf
Hugo do Prado <hugodopradofernandes@gmail.com>
2024-02-20 23:59:01 -0300

    Add files via upload

--- HomePage.py
+++ HomePage.py
@@ -1,7 +1,96 @@
 import streamlit as st
+import requests
+import langchain
+from langchain.chains import ConversationChain
+from langchain.memory import ConversationBufferMemory
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
+from langchain_community.embeddings import HuggingFaceEmbeddings as SentenceTransformerEmbeddings
+from langchain.llms.base import LLM
+from typing import Optional, List, Mapping, Any
+from io import StringIO
 
+#-------------------------------------------------------------------
+class webuiLLM(LLM):
+    @property
+    def _llm_type(self) -> str:
+        return "custom"
+
+    def _call(self, prompt: str, stop: Optional[List[str]] = None) -> str:
+        response = requests.post(
+            "http://127.0.0.1:5000/v1/completions",
+            json={
+                "prompt": prompt,
+                "max_tokens": 2048,
+                "do_sample": "false",
+                "temperature": 0.7,
+                "top_p": 0.1,
+                "typical_p": 1,
+                "repetition_penalty": 1.18,
+                "top_k": 40,
+                "min_length": 0,
+                "no_repeat_ngram_size": 0,
+                "num_beams": 1,
+                "penalty_alpha": 0,
+                "seed": -1,
+                "add_bos_token": "true",
+                "ban_eos_token": "false",
+                "skip_special_tokens": "false",
+                "stop": ["Human: "],
+            }
+        )
+
+        response.raise_for_status()
+
+        return response.json()["choices"][0]["text"].strip().replace("```", " ")
+     
+    @property
+    def _identifying_params(self) -> Mapping[str, Any]:
+        """Get the identifying parameters."""
+        return {
+
+        }
+#-------------------------------------------------------------------
+langchain.verbose = False
+
+# Callback just to stream output to stdout, can be removed
+callbacks = [StreamingStdOutCallbackHandler()]
+
+llm = webuiLLM()
+chain = ConversationChain(llm=llm, memory=ConversationBufferMemory(),verbose=True)
+
+#-------------------------------------------------------------------
 # Main page setup
-st.set_page_config(page_title="Ask your PDF or Wikipedia")
-st.header("Ask your PDF or Wikipedia 💬")
-st.write("Select a page on the side menu")
+st.set_page_config(page_title="LLM Wrapper")
+st.header("This is a LLM Wrapper 💬")
+st.write("Select a page on the side menu or use the chat below")
 st.sidebar.success("Select a page above.")
+
+#-------------------------------------------------------------------
+@st.cache_data(hash_funcs={StringIO: StringIO.getvalue},show_spinner="Prompting LLM...")
+def prompting_llm(prompt,_chain):
+    response = _chain.invoke(prompt,return_only_outputs=True).get("response")
+    return response
+#-------------------------------------------------------------------
+
+# Initialize chat history
+if "messages" not in st.session_state:
+    st.session_state.messages = []
+
+# Display chat messages from history on app rerun
+for message in st.session_state.messages:
+    with st.chat_message(message["role"]):
+        st.markdown(message["content"])
+
+# React to user input
+if prompt := st.chat_input("What is up?"):
+    # Display user message in chat message container
+    st.chat_message("user").markdown(prompt)
+    # Add user message to chat history
+    st.session_state.messages.append({"role": "user", "content": prompt})
+
+    response = prompting_llm(prompt,chain)
+    # Display assistant response in chat message container
+    with st.chat_message("assistant"):
+        st.markdown(response)
+    # Add assistant response to chat history
+    st.session_state.messages.append({"role": "assistant", "content": response})
