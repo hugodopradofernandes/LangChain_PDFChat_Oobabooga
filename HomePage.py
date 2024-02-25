2fd704ed9070ee3426f0bbf37aea36007ca550a1
Hugo do Prado <hugodopradofernandes@gmail.com>
2024-02-24 22:30:13 -0300

    Add files via upload

--- HomePage.py
+++ HomePage.py
@@ -19,7 +19,7 @@ class webuiLLM(LLM):
             "http://127.0.0.1:5000/v1/completions",
             json={
                 "prompt": prompt,
-                "max_tokens": 1024,
+                "max_tokens": 512,
                 "do_sample": "false",
                 "temperature": 0.7,
                 "top_p": 0.1,
@@ -74,28 +74,44 @@ callbacks = [StreamingStdOutCallbackHandler()]
 #Instantiate chat LLM and the search agent
 llm = webuiLLM()
 chain = ConversationChain(llm=llm, memory=ConversationSummaryMemory(llm=llm,max_token_limit=500), verbose=False)
+response = ""
 
 #-------------------------------------------------------------------
-@st.cache_data(show_spinner="Prompting LLM...")
 def prompting_llm(prompt,_chain):
-    response = _chain.invoke(prompt).get("response")
-    return response
+    with st.spinner(text="Prompting LLM..."):
+        response = _chain.invoke(prompt).get("response")
+        return response
 
 #-------------------------------------------------------------------
-def commands(prompt):
+def commands(prompt,last_prompt,last_response):
     match prompt.split(" ")[0]:
+        case "/continue":
+            prompt = "Given this question: " + last_prompt + ", continue the following text you already started: " + last_response.rsplit("\n\n", 3)[0]
+            response = prompting_llm(prompt,chain)
+            return response
+        
         case "/history":
-            return "Current History Summary:  \n" + chain.memory.load_memory_variables({"history"}).get("history")
-        case "/model":
-            headers = {'Accept': 'application/json'}
-            r = requests.get('http://127.0.0.1:5000/v1/internal/model/info', headers=headers)
-            r.raise_for_status()
-            return "Loaded model:  \n" + r.json()["model_name"]
+            try:
+                history = chain.memory.load_memory_variables({"history"}).get("history")
+                if history == "":
+                    return "No history to display"
+                else:
+                    return "Current History Summary:  \n" + history
+            except:
+                return "The history was cleared"
+
         case "/list":
             headers = {'Accept': 'application/json'}
             r = requests.get('http://127.0.0.1:5000/v1/internal/model/list', headers=headers)
             r.raise_for_status()
             return "Model list:  \n" + """{}""".format("  \n".join(r.json()["model_names"][0:]))
+            
+        case "/model":
+            headers = {'Accept': 'application/json'}
+            r = requests.get('http://127.0.0.1:5000/v1/internal/model/info', headers=headers)
+            r.raise_for_status()
+            return "Loaded model:  \n" + r.json()["model_name"]
+                   
         case s if s.startswith('/load'):
             model = prompt.split(" ")[1]
             headers = {'Accept': 'application/json'}
@@ -108,6 +124,10 @@ def commands(prompt):
                     return "Load command failed."
             else:
                 return "Model not in the list. Check the list with the /list command."
+            
+        case "/repeat":
+            return last_response
+        
         case "/stop":
             headers = {'Accept': 'application/json'}
             r = requests.post('http://127.0.0.1:5000/v1/internal/stop-generation', headers=headers)
@@ -118,17 +138,27 @@ def commands(prompt):
                 return "Stop command failed. Sometimes the LLM API becomes busy while generating text..."
             
         case "/help":
-            return "Comand list available: /history, /list, /stop, /help"
+            return "Comand list available: /continue, /history, /list, /model, /repeat, /stop, /help"
 
 #-------------------------------------------------------------------
 # Initialize chat history
 if "messages" not in st.session_state:
     st.session_state.messages = []
-if "buffer" not in st.session_state:
-    st.session_state.buffer = []
+if "history" not in st.session_state:
+    st.session_state.history = []
 else:
-    chain.memory = st.session_state.buffer
-
+    chain.memory = st.session_state.history
+if "last_response" not in st.session_state:
+    st.session_state.last_response = ""
+    last_response = ""
+else:
+    last_response = st.session_state.last_response
+if "last_prompt" not in st.session_state:
+    st.session_state.last_prompt = ""
+    last_prompt = ""
+else:
+    last_prompt = st.session_state.last_prompt
+    
 # Display chat messages from history on app rerun
 for message in st.session_state.messages:
     with st.chat_message(message["role"]):
@@ -142,7 +172,7 @@ if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
     
     if prompt.startswith("/"):
-        response = commands(prompt)
+        response = commands(prompt,last_prompt,last_response)
         # Display assistant response in chat message container
         with st.chat_message("assistant",avatar="🔮"):
             st.markdown(response)
@@ -151,9 +181,11 @@ if prompt := st.chat_input("What is up?"):
         # Display assistant response in chat message container
         with st.chat_message("assistant"):
             st.markdown(response)
-#         
+       
     # Add assistant response to chat history
     st.session_state.messages.append({"role": "assistant", "content": response})
 
 # Save chat history buffer to the session
-st.session_state.buffer = chain.memory
+st.session_state.history = chain.memory
+st.session_state.last_prompt = prompt
+st.session_state.last_response = response
